cf394ee0780b55e30bbadf40a2c2209c42f0e060
Norman Huth <norman@huth.it>
2025-07-25 23:26:16 +0200

    feat: add german translations

---
+++ resources/lang/de/filemanager.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'root_folder_not_configurated' => 'Dein Stammordner wurde nicht erstellt',
+    'root_folder_not_configurated_help' => 'Dieser Ordner wird als Stammordner deines Dateimanagers verwendet',
+    'add_your_first_folder' => 'Erstelle deinen ersten Ordner',
+    'root_folder_name' => 'Ordnername',
+    'elements' => '{0} Keine Elemente|[1] Element|[2,*] :value Elemente',
+    'add_a_file' => 'Datei hinzufügen',
+    'search' => 'Suche',
+    'folder_already_exists' => 'Ein Ordner mit diesem Namen existiert bereits im aktuellen Verzeichnis.',
+    'folder_without_title' => 'unbenannter Ordner',
+    'selected' => '[1] ausgewählt|[2,*] ausgewählt',
+    'search_results' => 'Suchergebnisse',
+    'created' => 'Erstellt',
+    'modified' => 'Geändert',
+    'delete_items' => 'Elemente löschen',
+    'delete_items_warning' => 'Bist du sicher, dass du diese Elemente löschen möchtest?',
+    'rename_folder' => 'Ordner umbenennen',
+    'rename_file' => 'Datei umbenennen',
+    'actions' => [
+        'cancel' => 'Abbrechen',
+        'save' => 'Speichern',
+        'delete' => 'Löschen',
+        'close_modal' => 'Modal schließen',
+        'copy_url' => 'Datei-URL kopieren',
+        'url_copy_pasted' => 'Link wurde in die Zwischenablage kopiert!',
+        'url_not_copy_pasted' => 'Die Zwischenablage-API wird in nicht sicheren Umgebungen nicht unterstützt. Bitte die Seite über HTTPS aufrufen.',
+    ],
+    'validation' => [
+        'folder_name_required' => 'Ein Ordnername ist erforderlich',
+    ],
+];
